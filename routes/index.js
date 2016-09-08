7a908a4a85c9bb1ed418f4cee26fcb95ed0bc502
jttwnsnd <jt.a.townsend@gmail.com>
2016-09-08 18:10:18 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var mongoClient = mongodb.MongoClient;
 var mongoUrl = 'mongodb://localhost:27017/electric';
 var db; //Global so all of our routes have access to our database connection
 
-mongoClient.connect(mongoUrl, function(error, database){
+mongoClient.connect(mongoUrl, (error, database) => {
 	if(error){
 		console.log(error)
 	}else{
@@ -15,11 +15,20 @@ mongoClient.connect(mongoUrl, function(error, database){
 		console.log(db);
 	}
 })
+router.get('/', (req, res, next)=> {
+	db.collection('images').find().toArray((error, allResults) => {
+		var standingsArray = [];
+		allResults.sort((a,b) => {
+			return (b.totalVotes - a.totalVotes);
+		});
+		res.render('index', {theStandings: allResults});
+	})
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/game', (req, res, next) => {
 	var userIP = req.ip;
-	db.collection('votes').find({ip:userIP}).toArray(function(error, userResult){
+	db.collection('votes').find({ip:userIP}).toArray((error, userResult) => {
 		var photosVoted = [];
 		if(error){
 			console.log("There was an error fetching user votes");
@@ -29,7 +38,7 @@ router.get('/', function(req, res, next) {
 			}
 		}
 		// 2. get pics from Mongo and store them in an array to pass to view
-		db.collection('images').find({ imgSrc: {$nin: photosVoted } }).toArray(function(error, photos){
+		db.collection('images').find({ imgSrc: {$nin: photosVoted } }).toArray((error, photos) => {
 		  	if(photos.length === 0){
 		  		res.redirect('/standings');
 		  	}else{
@@ -37,14 +46,14 @@ router.get('/', function(req, res, next) {
 			  	// 3. grab a random image from that array
 			  	var randomPhoto = photos[randomNum].imgSrc;
 			  	// 4. send that image to the view
-			  	res.render('index', { imageToRender: randomPhoto });
+			  	res.render('game', { imageToRender: randomPhoto });
 		  	}
 		  	
 		})
 	})
 });
 
-router.post('/electric', function(req, res, next){
+router.post('/electric', (req, res, next) => {
 	// res.json(req.body);
 	// 1. we know whether they voted electric or poser because it's in re.body.submit
 	// 2. we know what image they voted on because it is in req.body.image
@@ -55,82 +64,71 @@ router.post('/electric', function(req, res, next){
 		image: req.body.image
 	})
 	// 7. update the images collection so that the images voted on will have a new total votes
-	db.collection('images').find({imgSrc: req.body.image}).toArray(function(error, result){
-		var total, posers, elctrc;
-		var inputValue = req.body.submit;
-		console.log(result[0].totalVotes);
+	db.collection('images').find({imgSrc: req.body.image}).toArray((error, result) =>{
+		var total;
+		var upDownVote;
+		if(req.body.submit === "Electric!"){
+			upDownVote = 1;
+		}else if(req.body.submit === "Poser!"){
+			upDownVote = -1;
+		}
 		if(isNaN(result[0].totalVotes)){
 			total = 0;
 		}else{
 			total = result[0].totalVotes;
 		}
-		if(isNaN(result[0].poser)){
-			posers = 0;
-		}else{
-			posers = result[0].poser;
-		}
-		if(isNaN(result[0].electric)){
-			elctrc = 0;
-		}else{
-			elctrc = result[0].electric;
-		}
-		db.collection('images').updateOne(
-			{imgSrc: req.body.image},
-			{
-				$set: {'totalVotes': (total + 1)}
-			}, function(error, results){
-				//check to see if there is an error
-				if(error){
-					console.log(error)
-				}else{
-					console.log('updating');
-				}
-				//check to see if the document was updated
-			}
-		)
-		if(inputValue === "Electric!"){
-			db.collection('images').update(
+		if((upDownVote == -1) && (total === 0)){
+			db.collection('images').updateOne(
 				{imgSrc: req.body.image},
 				{
-					$set: {'electric': (elctrc + 1), 'poser': posers},
-				}, function(error, results){
+					$set: {'totalVotes': (0)}
+				}, (error, results) => {
 					//check to see if there is an error
 					if(error){
 						console.log(error)
 					}else{
-						console.log('electric');
+						console.log('updating');
 					}
 					//check to see if the document was updated
 				}
 			)
-		}else if(inputValue === "Poser!"){
-			db.collection('images').update(
+		}else{
+			db.collection('images').updateOne(
 				{imgSrc: req.body.image},
 				{
-					$set: {'poser': (posers + 1), 'electric': elctrc},
-				}, function(error, results){
+					$set: {'totalVotes': (total + upDownVote)}
+				}, (error, results) => {
 					//check to see if there is an error
 					if(error){
 						console.log(error)
 					}else{
-						console.log('poser');
+						console.log('updating');
 					}
 					//check to see if the document was updated
 				}
 			)
 		}
 	})
-	res.redirect('/');
+	res.redirect('/game');
 })
 
-router.get('/standings', function(req, res, next){
-	db.collection('images').find().toArray(function(error, allResults){
+router.get('/standings', (req, res, next) => {
+	db.collection('images').find().toArray((error, allResults) => {
 		var standingsArray = [];
-		allResults.sort(function(a,b){
+		allResults.sort((a,b) => {
 			return (b.totalVotes - a.totalVotes);
 		});
 		res.render('standings', {theStandings: allResults});
 	})
+})
+
+router.get('/resetUserValues', (req, res, next) => {
+	db.collection('votes').deleteMany(
+		{ip:req.ip},
+		(error, results) => {
+		}
+	)
+	res.redirect('/');
 })
 
 module.exports = router;
